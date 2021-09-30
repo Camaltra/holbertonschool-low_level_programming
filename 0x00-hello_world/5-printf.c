4ee4c4ff4682b60c7b634b7b873722135c51ac70
Camaltra <mickael.boillaud@gmail.com>
2021-09-30 09:08:16 +0000

    Modif 5 ex

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Write a simple message with printf 
+ * main - Write a simple message with printf
  * Return: 0 cause void function
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }

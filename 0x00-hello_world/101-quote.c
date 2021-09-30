e7dba872cf6cd7df4d5e8331ed44b07ee4950279
Camaltra <mickael.boillaud@gmail.com>
2021-09-30 12:58:35 +0000

    Modif on 9 ex

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Print a quote withou using printf or puts
+ * main - Print a quote without using printf or puts, using write from unistd.h
  * Return: 0 cause it's a void function
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }

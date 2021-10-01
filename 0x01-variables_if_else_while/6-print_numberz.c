531dc81bc2e7c6a8f4ea73968d69dd2ad1ad103f
Camaltra <mickael.boillaud@gmail.com>
2021-10-01 11:54:28 +0000

    Modif 6 ex

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - 
- * Return: 
+ * main - Print all the digit in base 10 using putchar
+ * Return: 0 void function
  */
 
 int main(void)
-{	
+{
 	int digit = 48;
 
 	while (digit < 58)

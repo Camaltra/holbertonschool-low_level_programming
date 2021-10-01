1c65417c7f34eefcbe4ced6cfc1792eb90430029
Camaltra <mickael.boillaud@gmail.com>
2021-10-01 12:59:21 +0000

    ex 10 added

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Print list of number, sorted by a certain sort, without using printf
+ * Return: 0 void function
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 48;
+	int e = 48;
+	int c = 49;
+
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			if (e < c)
+			{
+				e++;
+			}
+			else
+			{
+				putchar(i);
+				putchar(j);
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		e = 48;
+		c++;
+		i++;
+		j = 48;
+	}
+	putchar('\n');
+	return (0);
+}

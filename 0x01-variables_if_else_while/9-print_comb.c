fb1d6c250b18b275112d77637a58b4cd7fca5034
Camaltra <mickael.boillaud@gmail.com>
2021-10-01 12:22:59 +0000

    9 ex added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print all the digit in base 10 separate by , using putchar
+ * Return: 0 void function
+ */
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit < 58)
+	{
+		putchar(digit);
+		if (digit != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}

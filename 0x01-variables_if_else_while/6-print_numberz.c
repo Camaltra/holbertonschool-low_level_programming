0f0743c3895bed648e6d21610f7281ee6a9fdfa4
Camaltra <mickael.boillaud@gmail.com>
2021-10-01 11:51:52 +0000

    6 ex added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - 
+ * Return: 
+ */
+
+int main(void)
+{	
+	int digit = 48;
+
+	while (digit < 58)
+	{
+		putchar(digit);
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}

44094342c02fba531efab7d6ef26a2191dc84774
Camaltra <mickael.boillaud@gmail.com>
2021-10-01 09:30:51 +0000

    5 ex added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print all digit of a base 10 number
+ * Return: 0 cause void function
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		printf("%d", digit);
+		digit++;
+	}
+	putchar('\n');
+
+	return(0);
+}

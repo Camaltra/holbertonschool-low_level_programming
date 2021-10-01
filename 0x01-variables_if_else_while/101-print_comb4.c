0cf58a1cade7f0d5dbb8b1ea0f0280a886722d5a
Camaltra <mickael.boillaud@gmail.com>
2021-10-01 14:45:23 +0000

    Modif 101 ex

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,26 +6,31 @@
  */
 int main(void)
 {
-    int i, j, k;
+	int i, j, k;
 
-    i=0;
-    while (i < 10)
-    {
-        j=i+1;
-        while(j < 10)
-        {
-            k=j+1;
-            while(k < 10)
-            {
-                putchar(i+48);
-                putchar(j+48);
-                putchar(k+48);
-                putchar(' ');
-                k++;
-            }
-            j++;
-        }
-        i++;
-    }
+	i=0;
+	while (i < 10)
+	{
+		j= i + 1;
+		while(j < 10)
+		{
+			k= j + 1;
+			while(k < 10)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+				putchar(k + 48);
+				k++;
+				if (i != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
    return (0);
 }

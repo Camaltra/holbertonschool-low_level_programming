acfd29189b14618d03ff217485197abd5fa09b0a
Camaltra <mickael.boillaud@gmail.com>
2021-10-01 09:12:21 +0000

    4 ex added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lower case without q,e and using printf
+ * Return: 0 cause void function
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet);
+			alphabet++;
+		}
+		else
+		{
+			alphabet++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}

b9280cbdb92a39661a0f92fce429e2a63bbcb684
Camaltra <mickael.boillaud@gmail.com>
2021-10-05 08:28:19 +0000

    Task 1 added

---
+++ 0x02-functions-nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * alphabet - Print alphabet in lowercase using our function _putchar
+ *
+ * Return : 0 (succes)
+ */
+void print_alphabet(void)
+{
+	char alphabet= 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+        alphabet++;
+	}
+	_putchar('\n');
+}

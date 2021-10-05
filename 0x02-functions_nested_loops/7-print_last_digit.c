e354ff4148b010dde1ed6b3bb8cc827d1f6c7376
Camaltra <mickael.boillaud@gmail.com>
2021-10-05 12:11:47 +0000

    Modification on Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,14 +2,15 @@
 
 /**
  * print_last_digit - Isolate the last digit of a integer, and print it
- * 
- * @N: Integer input 
- * 
+ *
+ * @N: Integer input
+ *
  * Return: The last digits
  */
 int print_last_digit(int N)
 {
-    (N < 0) ? N = -N : N; 
-    _putchar(N % 10 + '0');
-    return(N % 10);
+	(N < 0) ? N = -1 * N : N;
+	_putchar('0' + N % 10);
+	return (N % 10);
 }
+

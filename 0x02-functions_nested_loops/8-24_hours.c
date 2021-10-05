5285cd5db0b492648ba151a3a5e60b2f83531d9d
Camaltra <mickael.boillaud@gmail.com>
2021-10-05 12:58:27 +0000

    Modification on documentation

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * 
- * 
- * 
- * 
+ * jack_bauer - Print all minutes, from 00:00 to 23:59
+ *
+ * Return: Anything, void function
  */
 void jack_bauer(void)
 {
-    int i = 0, j;
-    while (i < 24)
-    {
-        j = 0;
-        while (j < 60)
-        {
-            _putchar((i / 10) + 48);
-            _putchar((i % 10) + 48);
-            _putchar(':');
-            _putchar((j / 10) + 48);
-            _putchar((j % 10) + 48);
-            _putchar('\n');
-            j++;
-        }
-    i++;
-    }
+	int i = 0, j;
+
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+			_putchar(':');
+			_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+			_putchar('\n');
+			j++;
+		}
+	i++;
+	}
 }

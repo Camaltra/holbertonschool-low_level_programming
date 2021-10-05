d6bc50543628282b1d8484a9479413690c2c6155
Camaltra <mickael.boillaud@gmail.com>
2021-10-05 09:46:05 +0000

    Modification name directory

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Print "_putchar" using our function _putchar, included in main.h.
+ *
+ * Return : 0 (succes).
+ */
+int main(void)
+{
+	char text[8] = "_putchar";
+	int i = 0;
+
+	while (text[i] != '\0')
+	{
+		_putchar(text[i++]);
+	}
+	_putchar('\n');
+
+	return (0);
+}

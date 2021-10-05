53f41b339d461bd63455b508dd4cebecd764685b
Camaltra <mickael.boillaud@gmail.com>
2021-10-05 13:32:20 +0000

    Modifications on Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,13 @@
 
 /**
  * add - Add two integer.
- * 
+ *
+ * @a: Integer to add
+ * @b: Integer to add
+ *
  * Return: The result of these addition.
  */
 int add(int a, int b)
 {
-    return (a + b); 
+	return (a + b);
 }

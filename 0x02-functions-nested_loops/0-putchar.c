ff870f4a818010299113494b7a4dc88751580ab3
Camaltra <mickael.boillaud@gmail.com>
2021-10-05 07:58:20 +0000

    Modification on task 1

--- 0x02-functions-nested_loops/0-putchar.c
+++ 0x02-functions-nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include "_putchar.c"
+#include "main.h"
 
 /**
  * main - Print "_putchar" using our function _putchar
  *
  * Return : 0 (succes)
  */
-
 int main(void)
 {
 	char text[8] = "_putchar";
-	int i = 0;
+	int i = -1;
 
-	while (i != 7)
+	while (i != 8)
 	{
-		_putchar(text[i]);
-		i++;
+		_putchar(text[i++]);
 	}
+	_putchar('\n');
 
 	return (0);
 }

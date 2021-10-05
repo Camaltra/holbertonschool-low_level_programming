6f6f63da1db48062a78bb62873d3950163ff43af
Camaltra <mickael.boillaud@gmail.com>
2021-10-05 21:45:48 +0000

    Modification on Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -24,10 +24,10 @@ int main(void)
 		n2 = n3;
 		n3 = n1 + n2;
 	}
-	n2a = n2 / 10000000000;
-	n2b = n2 % 10000000000;
-	n3a = n3 / 10000000000;
-	n3b = n3 % 10000000000;
+	n2a = n2 / 1000000000;
+	n2b = n2 % 1000000000;
+	n3a = n3 / 1000000000;
+	n3b = n3 % 1000000000;
 	for (j = 89; j < 98; j++) /* start to cut the long in half */
 	{
 		printf("%li%li, ", n3a, n3b);
@@ -35,8 +35,8 @@ int main(void)
 		n1b = n2b;
 		n2a = n3a;
 		n2b = n3b;
-		n3a = n1a + n2a + ((n1b + n2b) / 10000000000);
-		n3b = (n1b + n2b) % 10000000000;
+		n3a = n1a + n2a + ((n1b + n2b) / 1000000000);
+		n3b = (n1b + n2b) % 1000000000;
 	}
 	printf("%li%li\n", n3a, n3b);
 	return (0);

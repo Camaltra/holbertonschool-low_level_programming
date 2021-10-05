be20f2bd677c78e56a9cf0b5b4dce2f6889344d7
Camaltra <mickael.boillaud@gmail.com>
2021-10-05 21:44:21 +0000

    Modification on Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,9 +8,7 @@
  */
 int main(void)
 {
-	unsigned long int n1, n1a, n1b, n2, n2a, n2b, n3, n3a, n3b;
-	int i = 3;
-	int j = 89;
+	unsigned long int n1, n1a, n1b, n2, n2a, n2b, n3, n3a, n3b, i, j;
 
 	n1 = 1;
 	n2 = 2;
@@ -19,19 +17,18 @@ int main(void)
 
 	printf("%li, ", n1);
 	printf("%li, ", n2);
-	while (i < 89)  /* unsigned int storage stop here*/
+	for (i = 3; i < 89; i++)  /* unsigned int storage stop here*/
 	{
 		printf("%li, ", n3);
 		n1 = n2;
 		n2 = n3;
 		n3 = n1 + n2;
-		i++;
 	}
 	n2a = n2 / 10000000000;
 	n2b = n2 % 10000000000;
 	n3a = n3 / 10000000000;
 	n3b = n3 % 10000000000;
-	while (j < 98) /* start to cut the long in half */
+	for (j = 89; j < 98; j++) /* start to cut the long in half */
 	{
 		printf("%li%li, ", n3a, n3b);
 		n1a = n2a;
@@ -40,7 +37,6 @@ int main(void)
 		n2b = n3b;
 		n3a = n1a + n2a + ((n1b + n2b) / 10000000000);
 		n3b = (n1b + n2b) % 10000000000;
-		j++;
 	}
 	printf("%li%li\n", n3a, n3b);
 	return (0);

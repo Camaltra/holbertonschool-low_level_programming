9c02d85ffd031f17fd436354f7df5deae59cdbde
Camaltra <mickael.boillaud@gmail.com>
2021-10-07 09:55:47 +0000

    Task 11 added

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print the largest prime factor of 612852475143.
+ *
+ * Return: The largest prime factor of 612852475143
+ */
+int main(void)
+{
+	long primeFactor = 2, number = 612852475143;
+	int mLPN = 0;
+
+	while (number > 1)
+	{
+		while (number % primeFactor == 0)
+		{
+			mLPN = primeFactor;
+			number = number / primeFactor;
+		}
+		primeFactor++;
+	}
+	printf("%d\n", mLPN);
+	return (mLPN);
+}

3f7a38054502026a0574dd66a683b71076844688
Camaltra <mickael.boillaud@gmail.com>
2021-10-07 00:06:20 +0000

    Task 5 added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print all the numbers between 0-14 10 times
+ *
+ * Return: Anything, cause void function
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}

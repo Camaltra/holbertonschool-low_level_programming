1aa66d4490f5582ba6f04e8f21870f2624c69c08
Camaltra <mickael.boillaud@gmail.com>
2021-10-10 23:22:45 +0000

    Modification, remove '\0' at the begin

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,9 +14,9 @@ void print_rev(char *s)
 {
 	int length;
 
-	for (length = _strlen(s); length >= 0; length--)
+	for (length = _strlen(s) - 1; length >= 0; length--)
 	{
-		_putchar(s[length - 1]);
+		_putchar(s[length]);
 	}
 	_putchar('\n');
 }

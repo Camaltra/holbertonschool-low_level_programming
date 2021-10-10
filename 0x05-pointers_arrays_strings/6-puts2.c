1889dbd7d975c0476fd5924de9347cd08d8c6368
Camaltra <mickael.boillaud@gmail.com>
2021-10-10 23:57:50 +0000

    Task 6 added

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * puts2 - Prints every other character of a string,
+ * starting with the first character.
+ *
+ * @str: inputed string.
+ *
+ * Return: Anything, cause void function.
+ */
+void puts2(char *str)
+{
+	int length;
+
+	for (length = 0; length < _strlen(str); length++)
+	{
+		if (length % 2 == 0)
+		{
+			_putchar(str[length]);
+		}
+	}
+	_putchar('\n')
+}
+
+/**
+ * _strlen - Calculate the length of a string.
+ *
+ * @s: String to manipulate.
+ *
+ * Return: The length of the inputed string.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}

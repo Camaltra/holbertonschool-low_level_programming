6cdace356edb672685d8d5e838e1229f4d698f4b
Camaltra <mickael.boillaud@gmail.com>
2021-10-11 00:43:53 +0000

    Task 9 added

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ *
+ * @dest: Destination
+ * @src: Source
+ *
+ * Return: Anything, cause void function.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int counter, length = _strlen(src);
+
+	for (counter = 0; counter < length; counter++)
+	{
+		dest[counter] = src[counter];
+	}
+	return (dest);
+}
+
+/**
+ * _strlen - Calculate the length of a string.
+ *
+ * @s: String to manipulate.
+ *
+ * Return: The length of the inputed string.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}

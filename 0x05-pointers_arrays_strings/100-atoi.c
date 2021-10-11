54d774f702072174d004c6455c0d46da48eb636a
Camaltra <mickael.boillaud@gmail.com>
2021-10-11 10:12:30 +0000

    Add betty style, and correct the - behin number

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,45 +4,46 @@ int _strlen(char *s);
 int _atoi(char *s);
 
 /**
- * 
- * 
- * 
- * 
- * 
+ * _atoi - Convert the first number of a str to an interger.
+ *
+ * @s: String inputed.
+ *
+ * Return: 0 if there are no number, the interger based on the number.
  */
 int _atoi(char *s)
 {
-    int counter, sign = 1; 
-    unsigned int num = 0; 
+	int counter, sign = 1;
+	unsigned int num = 0;
 
-    for (counter = 0; s[counter] != '\0'; counter++)
-    {
-        if (s[counter] >= '0' && s[counter] <= '9')
-        {
-            if (s[counter -1] == '-')
-            {
-                sign = - 1;
-            }
-            break;
-        }
-    }
-    if (counter == _strlen(s) - 1)
-    {
-        return (0);
-    }
-    else 
-    {
-        while (s[counter] >= '0' && s[counter] <= '9')
-        {
-            num = num*10 + s[counter] - '0';
-            counter++;
-        }
-    if (sign == - 1)
-    {
-        num = - num;
-    }
-    return (num);
-    }
+	for (counter = 0; s[counter] != '\0'; counter++)
+	{
+		if (s[counter] == '-')
+		{
+			sign *= -1;
+		}
+		if (s[counter] >= '0' && s[counter] <= '9')
+		{
+			break;
+		}
+	}
+	if (counter == _strlen(s) - 1)
+	{
+		return (0);
+	}
+	else
+	{
+		while (s[counter] >= '0' && s[counter] <= '9')
+		{
+			num = num * 10 + s[counter] - '0';
+			counter++;
+		}
+	}
+	if (sign == -1)
+	{
+		num = -num;
+	}
+	return (num);
+	}
 }
 
 

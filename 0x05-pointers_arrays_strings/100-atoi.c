56a4dfefdf16f8bb5b4aafcfcf921fdd28ec949b
Camaltra <mickael.boillaud@gmail.com>
2021-10-11 10:39:59 +0000

    Change totally the form of the program, only 2 loops

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,5 @@
 #include "main.h"
-
-int _strlen(char *s);
-int _atoi(char *s);
+#include <stdio.h>
 
 /**
  * _atoi - Convert the first number of a str to an interger.
@@ -12,54 +10,21 @@ int _atoi(char *s);
  */
 int _atoi(char *s)
 {
-	int counter, sign = 1;
+	int counter = 0, sign = 1;
 	unsigned int num = 0;
 
-	for (counter = 0; s[counter] != '\0'; counter++)
+	while ((s[counter] > '9' || s[counter] < '0') && s[counter] != '\0')
 	{
 		if (s[counter] == '-')
 		{
 			sign *= -1;
 		}
-		if (s[counter] >= '0' && s[counter] <= '9')
-		{
-			break;
-		}
+		counter++;
 	}
-	if (counter == _strlen(s) - 1)
+	while (s[counter] <= '9' && s[counter] >= '0')
 	{
-		return (0);
+		num = num * 10 + (s[counter] - '0');
+		counter++;
 	}
-	else
-	{
-		while (s[counter] >= '0' && s[counter] <= '9')
-		{
-			num = num * 10 + s[counter] - '0';
-			counter++;
-		}
-	}
-	if (sign == -1)
-	{
-		num = -num;
-	}
-	return (num);
-}
-
-
-/**
- * _strlen - Calculate the length of a string.
- *
- * @s: String to manipulate.
- *
- * Return: The length of the inputed string.
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	}
-	return (i);
+	return (num * sign);
 }

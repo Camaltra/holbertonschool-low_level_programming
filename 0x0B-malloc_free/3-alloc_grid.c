0697ea0644420bef82a7dc3e7bdf518ba15d58a1
Camaltra <mickael.boillaud@gmail.com>
2021-10-26 10:05:14 +0000

    Modification on task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,6 +18,9 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	array = (int **)malloc(height * sizeof(int *));
+
+	if (array == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		array[i] = (int *)malloc(width * sizeof(int));

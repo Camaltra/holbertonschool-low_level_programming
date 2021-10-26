3de36aaf27e5c5463effab50467f131d80408fe3
Camaltra <mickael.boillaud@gmail.com>
2021-10-26 09:47:30 +0000

    Task 3 added

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+int **alloc_grid(int width, int height)
+{
+    int **array;
+    int i, j;
+
+    if (!(width > 0) || !(height > 0))
+        return (NULL);
+    
+    array = (int **)malloc((width * height) * sizeof(int*));
+    for (i = 0; i < height; i++)
+    {
+        array[i] = (int *)malloc(width * sizeof(int));
+        for (j = 0; j < width; j++)
+        {
+           array[i][j] = 0; 
+        }
+    }
+    return (array);
+
+}

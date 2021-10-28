eca2ed3f663e732de34cee8f38e70d8ee40786ce
Camaltra <mickael.boillaud@gmail.com>
2021-10-28 10:18:35 +0000

    Modification on task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,7 @@
 int _strlen(char *s);
 
 /**
- * str_concat - Concatenate 2 string into a new one
+ * string_nconcat - Concatenate 2 string into a new one
  *
  * @s1: Str one
  * @s2: Str two
@@ -40,7 +40,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		*(sFinal + i) = *(s1 + i);
 	for (i = 0; i < length2; i++)
 		*(sFinal + length1 + i) = *(s2 + i);
-	*(sFinal + totalLenght) = '\0';
+	*(sFinal + length1 + length2) = '\0';
 	return (sFinal);
 
 }

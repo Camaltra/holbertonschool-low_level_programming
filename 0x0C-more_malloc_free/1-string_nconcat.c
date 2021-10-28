fd04c7ccfd705b9f4e12c30f895f344ea5b2c5cb
Camaltra <mickael.boillaud@gmail.com>
2021-10-28 11:00:14 +0000

    Modification on task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < length2)
 		length2 = n;
 
-	sFinal = malloc(sizeof(char) * (length1 + length2 + 1));
+	sFinal = malloc(sizeof(char) * (length1 + length2 + 2));
 	if (sFinal == NULL)
 		return (NULL);
 

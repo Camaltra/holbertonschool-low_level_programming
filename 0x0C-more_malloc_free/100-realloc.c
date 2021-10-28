b6871ba4ae1b1e70b6c07a608eaca97b9a9a2c06
Camaltra <mickael.boillaud@gmail.com>
2021-10-28 10:55:20 +0000

    Modification on task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,6 +18,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	void *newArray;
 	int length = old_size;
 
+	if (ptr == NULL)
+		return(malloc(new_size));
+
 	if (new_size == old_size)
 		return (ptr);
 

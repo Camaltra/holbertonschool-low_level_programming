ba01988ccc7bb5e5b7d089a690e4d44211f5ec61
Camaltra <camaltra38@gmail.com>
2021-11-16 11:13:22 +0000

    Adding all mandatory task

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+* insert_nodeint_at_index - Insert a node at a given index
+*
+* @head: Pointer to the list
+* @idx: Index
+* @n: Value to fill
+*
+* Return: The node, NULL if any issues
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *browse, *new;
+	unsigned int i;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	browse = *head;
+
+	for (i = 0; i < idx - 1; i++)
+	{
+		if (browse->next == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+		browse = browse->next;
+	}
+
+	new->next = browse->next;
+	browse->next = new;
+	return (new);
+}

60c2249eee289e6cb75020eb8afdadd1da8fc32c
Camaltra <camaltra38@gmail.com>
2021-11-22 12:38:50 +0000

    Adding task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* read_textfile - Read n char of a file text
+*
+* @filename: File
+* @letters: Number of letter to read
+*
+* Return: The number of char printed.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file, size, size_write;
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+
+	size = read(file, buffer, letters);
+	size_write = write(1, buffer, size);
+
+	if (size == -1 || size != size_write)
+		return (0);
+
+	free(buffer);
+	close(file);
+	return (size);
+}

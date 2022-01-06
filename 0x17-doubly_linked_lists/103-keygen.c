ea8683eead31a3616f9fe79a29ef2b5c713e2897
Camaltra <mickael.boillaud@gmail.com>
2022-01-06 14:27:00 +0000

    Betty like it

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -13,23 +13,23 @@
 int main(int argc, char **argv)
 {
 	int lenName = 0;
-	char *listCode = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char *lC = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	int i, sum, _char;
 	char *res = malloc(sizeof(char) * 7);
-	
+
 	if (argc != 2 || !res)
 	{
 		printf("Only 2 argument is needed : ./keygen5 name");
 		return (-1);
 	}
 	lenName = strlen(argv[1]);
-	res[0] = listCode[(lenName ^ 59) & 63];
+	res[0] = lC[(lenName ^ 59) & 63];
 	for (i = 0, sum = 0; i < lenName; i++)
 		sum += argv[1][i];
-	res[1] = listCode[(sum ^ 79) & 63];
+	res[1] = lC[(sum ^ 79) & 63];
 	for (i = 0, sum = 1; i < lenName; i++)
 		sum *= argv[1][i];
-	res[2] = listCode[(sum ^ 85) & 63];
+	res[2] = lC[(sum ^ 85) & 63];
 	_char = argv[1][0];
 	for (i = 0; i < lenName; i++)
 	{
@@ -37,13 +37,13 @@ int main(int argc, char **argv)
 			_char = argv[1][i];
 	}
 	srand(_char ^ 14);
-	res[3] = listCode[rand() & 63];
+	res[3] = lC[rand() & 63];
 	for (i = 0, sum = 0; i < lenName; i++)
 		sum = sum + argv[1][i] * argv[1][i];
-	res[4] = listCode[(sum ^ 239) & 63];
+	res[4] = lC[(sum ^ 239) & 63];
 	for (i = 0, sum = 0; i < argv[1][0]; i++)
 		sum = rand();
-	res[5] = listCode[(sum ^ 229) & 63];
+	res[5] = lC[(sum ^ 229) & 63];
 	res[6] = '\0';
 	printf("%s\n", res);
 	return (0);

f2752f17601dcb9f3a81e9a2d67bdbdc728c1487
Camaltra <mickael.boillaud@gmail.com>
2022-03-17 11:03:47 +0000

    Fix task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
+"""
+Task 5
+Island perimeter
+"""
 
 
 def island_perimeter(grid):
-    area = 0
+    """
+    Determine the perimeter of a island
+    Args:
+        grid: 2D array that represente the ocean with the island
+    """
+    perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                area += 4
+                perimeter += 4
                 if i > 0 and grid[i - 1][j]:
-                    area -= 2
+                    perimeter -= 2
                 if j > 0 and grid[i][j - 1]:
-                    area -= 2
-    return area
+                    perimeter -= 2
+    return perimeter

48e75654ebacafea014c5699cfacc57f69efb7a2
Camaltra <mickael.boillaud@gmail.com>
2022-05-02 12:48:48 +0000

    Adding last tasks

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    skiplist_t *start, *end;
+
+    if (!list)
+        return (NULL);
+    
+    start = list;
+    end = list->express;
+	printf("Value checked at index [%ld] = [%d]\n", end->index, end->n);
+    while (end->next && end->n < value)
+    {
+        start = end;
+        if (end->express)
+            end = end->express;
+        else
+        {
+            while (end->next)
+                end = end->next;
+        }
+        printf("Value checked at index [%ld] = [%d]\n", end->index, end->n);
+    }
+    printf("Value found between indexes [%ld] and [%ld]\n",
+					start->index, end->index);
+    while (start && start->index <= end->index)
+	{
+		printf("Value checked at index [%ld] = [%d]\n", start->index, start->n);
+		if (start->n == value)
+			return (start);
+		start = start->next;
+	}
+    return (NULL);
+}
